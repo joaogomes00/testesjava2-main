9048ad558d5de42311d24959ed9424e0075a02f7
João Pedro <joao.lemos@soulasalle.com.br>
2022-12-06 17:30:04 -0300

    Initial commit

---
+++ bin/src/test/java/TesteCalc.java
@@ -0,0 +1,17 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class TesteCalc {
+	
+	public calculadora cal = new calculadora();
+	@Test
+	public void test() {
+		
+		double soma = cal.somar(2,3);
+		assertEquals(5,soma);
+	}
+
+}
